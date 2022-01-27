2f0d8cd92381617f6508e9d38f645b03ca2f4b2a
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2022-01-27 13:04:24 +0530

    Update: adding gitignore and docker compose for running the project
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+	
 	_ "github.com/gnanakeethan/kidney-registry/routers"
 	
 	"github.com/beego/beego/v2/client/orm"
@@ -9,10 +11,17 @@ import (
 )
 
 func main() {
-	orm.RegisterDataBase("default", "postgres", beego.AppConfig.DefaultString("sqlconn", ""))
+	orm.RegisterDataBase("default", "postgres", envConfig("sqlconn", ""))
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
 	beego.Run()
+}
+
+func envConfig(configField, defaultValue string) string {
+	if os.Getenv(configField) != "" {
+		return os.Getenv(configField)
+	}
+	return beego.AppConfig.DefaultString("sqlconn", defaultValue)
 }
