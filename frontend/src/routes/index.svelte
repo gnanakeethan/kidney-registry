c78a143582be7c5ca6dcbbf4e2e8dd4f2b8ca9c8
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2022-02-05 11:31:16 +0530

    Validating font
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,16 +1,21 @@
 <script>
-	import { userType } from '$lib/state/auth';
+    import {userType} from '$lib/state/auth';
 
-	let userTypeLocal = '';
-	userType.subscribe((value) => {
-		userTypeLocal = value;
-	});
+    let userTypeLocal = '';
+    userType.subscribe((value) => {
+        userTypeLocal = value;
+    });
 </script>
 
 <div>
-	<div>
-		<div class="h-5 p-4">
-			<p>Testing</p>
-		</div>
-	</div>
+    <div>
+        <div class="h-5 p-4 grid grid-cols-4 w-1/6">
+            <div class="font-serif">Testing</div>
+            <div>Testing</div>
+            <div class="font-sans">Testing</div>
+            <div class="font-serif">Testing</div>
+            <div>Testing</div>
+            <div class="font-serif">Testing</div>
+        </div>
+    </div>
 </div>
