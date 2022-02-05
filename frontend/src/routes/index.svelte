046ddd3aa6396128d3702a1752eed8b70ab37e9e
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2022-02-05 07:44:08 +0530

    Project
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,16 +1,16 @@
 <script>
-	import { userType } from '$lib/state/auth';
+    import {userType} from '$lib/state/auth';
 
-	let userTypeLocal = '';
-	userType.subscribe(value => {
-		userTypeLocal = value;
-	});
-
-	// function switchUserType() {
-	// 	if (userTypeLocal === 'advertiser') {
-	// 		userType.set('publisher');
-	// 	} else {
-	// 		userType.set('advertiser');
-	// 	}
-	// }
+    let userTypeLocal = '';
+    userType.subscribe(value => {
+        userTypeLocal = value;
+    });
 </script>
+<div>
+    <div>
+        <div class="m-2        h-5 m-3 p-4">
+            <div>
+            </div>
+        </div>
+    </div>
+</div>
