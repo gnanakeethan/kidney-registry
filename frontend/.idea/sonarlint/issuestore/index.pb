56539fc19edc9df999d9c993eccfa394ac413cf3
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2022-02-05 09:48:08 +0530

    Configuring prettier
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

--- frontend/.idea/sonarlint/issuestore/index.pb
+++ frontend/.idea/sonarlint/issuestore/index.pb
@@ -5,7 +5,7 @@
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 <
 package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
-G
-src/routes/index.svelte,5/1/51bcc514dca4100151288caa29d812033b5c875a
 ;
 .prettierrc,2/0/20137a9e94744624390b6748bc96df5ca1579b71
+G
+src/routes/index.svelte,5/1/51bcc514dca4100151288caa29d812033b5c875a
--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,16 +1,16 @@
 <script>
-    import {userType} from '$lib/state/auth';
+	import { userType } from '$lib/state/auth';
 
-    let userTypeLocal = '';
-    userType.subscribe((value) => {
-        userTypeLocal = value;
-    });
+	let userTypeLocal = '';
+	userType.subscribe((value) => {
+		userTypeLocal = value;
+	});
 </script>
 
 <div>
-    <div>
-        <div class="m-3 h-5 p-4 m-4">
-            <div/>
-        </div>
-    </div>
+	<div>
+		<div class="m-4 m-2 h-5 p-4">
+			<div />
+		</div>
+	</div>
 </div>
