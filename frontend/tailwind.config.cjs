8339b37c0728a71f8102df07d425cfb580690d11
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2022-02-06 16:34:31 +0530

    Adding data grid implementation
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -5,7 +5,17 @@ module.exports = {
     ],
     theme: {
         extend: {
-            colors: {},
+            colors: {
+                'light-blue1': '#dbf0fe',
+                'light-blue2': '#00c0e0',
+                'light-blue3': '#2e90ff',
+                'light-blue4': '#0b84c4',
+                'light-blue5': '#0b84c4',
+                'light-blue6': '#e73c40',
+                'light-blue7': '#faa41a',
+                'light-blue8': '#fedeb6',
+                'light-blue9': '#008200',
+            },
             fontFamily: {
                 'serif': ['Playfair Display', 'ui-serif', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif'],
                 'sans': ['Open Sans', 'ui-sans', 'Arial', 'Helvetica', 'sans-serif'],
