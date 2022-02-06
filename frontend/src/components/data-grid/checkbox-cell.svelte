f1e9ae3d7f9becfc5fc50c2d4d1544d8f190407f
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2022-02-06 16:40:27 +0530

    Include datagrid as internal dep
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

---
+++ frontend/src/components/data-grid/checkbox-cell.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+	export let checkbox = null;
+	export let row;
+	export let column;
+	export let rowNumber;
+	// [svelte-upgrade suggestion]
+	// review these functions and remove unnecessary 'export' keywords
+	export function onChange(event) {
+		// delay this until after the ui updates on the screen
+		setTimeout(() => {
+			dispatch('valueupdate', {
+				row,
+				column,
+				value: checkbox.checked,
+				rowNumber
+			});
+		}, 0);
+	}
+</script>
+
+<div class="checkbox-cell">
+	<input
+		bind:this={checkbox}
+		checked={row.data[column.dataName]}
+		on:click={onChange}
+		type="checkbox"
+	/>
+</div>
+
+<style>
+	.checkbox-cell {
+		text-align: center;
+		background: white;
+	}
+</style>
