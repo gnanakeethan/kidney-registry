9574c3cda6d009f11dffe79b5d0e4ef77ff96def
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2022-02-08 15:49:24 +0530

    Update: setup client
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

--- frontend/src/lib/api/repository.ts
+++ frontend/src/lib/api/repository.ts
@@ -1,7 +1,30 @@
+import { createClient } from '@urql/svelte';
 import { QueryRepository } from 'lib/interfaces/repository';
+import { authState } from 'lib/state/auth';
+import { Client } from 'urql';
+
+let auth = { loggedIn: false, token: '', loginAs: null };
+authState.subscribe((authStateS) => (auth = authStateS));
 
 export class GraphQLQueryRepository<T> implements QueryRepository<T> {
+	private client: Client;
+
+	constructor() {
+		this.client = createClient({
+			url: 'http://localhost:3000/graphql',
+			fetchOptions: () => {
+				if (auth.token.length > 0) {
+					return {
+						headers: { authorization: auth.token.length > 0 ? `Bearer ${auth.token}` : '' }
+					};
+				}
+				return {};
+			}
+		});
+	}
+
 	getItems(query: string, page: number, perPage: number): T[] {
+		// this.client.query(query);
 		return [];
 	}
 }
