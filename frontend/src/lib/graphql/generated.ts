9dee1db1ff428233e2d8daef0f551cda2af2dbf7
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2022-02-09 09:26:13 +0530

    Generating graphql
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

--- frontend/src/lib/graphql/generated.ts
+++ frontend/src/lib/graphql/generated.ts
@@ -1,183 +1,156 @@
-import type { ApolloQueryResult, ObservableQuery, WatchQueryOptions } from '@apollo/client';
 import gql from 'graphql-tag';
-import type { Readable } from 'svelte/store';
-import { readable } from 'svelte/store';
-import client from 'undefined';
-
+import * as Urql from 'urql';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-	ID: string;
-	String: string;
-	Boolean: boolean;
-	Int: number;
-	Float: number;
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
 };
 
 export type DashboardMenus = {
-	__typename?: 'DashboardMenus';
-	sidebarBottom?: Maybe<Menu>;
-	sidebarTop?: Maybe<Menu>;
+  __typename?: 'DashboardMenus';
+  sidebarBottom?: Maybe<Menu>;
+  sidebarTop?: Maybe<Menu>;
 };
 
 export type Error = {
-	__typename?: 'Error';
-	status: Scalars['Int'];
-	string: Scalars['String'];
+  __typename?: 'Error';
+  status: Scalars['Int'];
+  string: Scalars['String'];
 };
 
 export type IntFilter = {
-	and?: InputMaybe<IntFilter>;
-	beginsWith?: InputMaybe<Scalars['Boolean']>;
-	endsWith?: InputMaybe<Scalars['Boolean']>;
-	eq?: InputMaybe<Scalars['Boolean']>;
-	gt?: InputMaybe<Scalars['Boolean']>;
-	gte?: InputMaybe<Scalars['Boolean']>;
-	lt?: InputMaybe<Scalars['Boolean']>;
-	lte?: InputMaybe<Scalars['Boolean']>;
-	or?: InputMaybe<IntFilter>;
+  and?: InputMaybe<IntFilter>;
+  beginsWith?: InputMaybe<Scalars['Boolean']>;
+  endsWith?: InputMaybe<Scalars['Boolean']>;
+  eq?: InputMaybe<Scalars['Boolean']>;
+  gt?: InputMaybe<Scalars['Boolean']>;
+  gte?: InputMaybe<Scalars['Boolean']>;
+  lt?: InputMaybe<Scalars['Boolean']>;
+  lte?: InputMaybe<Scalars['Boolean']>;
+  or?: InputMaybe<IntFilter>;
 };
 
 export type Menu = {
-	__typename?: 'Menu';
-	items?: Maybe<Array<Maybe<MenuItem>>>;
+  __typename?: 'Menu';
+  items?: Maybe<Array<Maybe<MenuItem>>>;
 };
 
 export type MenuItem = {
-	__typename?: 'MenuItem';
-	icon?: Maybe<Scalars['String']>;
-	name?: Maybe<Scalars['String']>;
-	route?: Maybe<Scalars['String']>;
+  __typename?: 'MenuItem';
+  icon?: Maybe<Scalars['String']>;
+  name?: Maybe<Scalars['String']>;
+  route?: Maybe<Scalars['String']>;
 };
 
 export type Mutation = {
-	__typename?: 'Mutation';
-	error?: Maybe<Error>;
-	userLogin?: Maybe<UserToken>;
+  __typename?: 'Mutation';
+  error?: Maybe<Error>;
+  userLogin?: Maybe<UserToken>;
 };
 
+
 export type MutationUserLoginArgs = {
-	userLogin?: InputMaybe<UserLogin>;
+  userLogin?: InputMaybe<UserLogin>;
 };
 
 export type Pagination = {
-	__typename?: 'Pagination';
-	currentPage?: Maybe<Scalars['Int']>;
-	itemsPerPage?: Maybe<Scalars['Int']>;
-	nextPage?: Maybe<Scalars['Int']>;
-	prevPage?: Maybe<Scalars['Int']>;
-	totalItems?: Maybe<Scalars['Int']>;
+  __typename?: 'Pagination';
+  currentPage?: Maybe<Scalars['Int']>;
+  itemsPerPage?: Maybe<Scalars['Int']>;
+  nextPage?: Maybe<Scalars['Int']>;
+  prevPage?: Maybe<Scalars['Int']>;
+  totalItems?: Maybe<Scalars['Int']>;
 };
 
 export type Query = {
-	__typename?: 'Query';
-	error?: Maybe<Error>;
-	users?: Maybe<UserList>;
+  __typename?: 'Query';
+  error?: Maybe<Error>;
+  users?: Maybe<UserList>;
 };
 
+
 export type QueryUsersArgs = {
-	filter?: InputMaybe<UserListFilter>;
-	limit?: InputMaybe<Scalars['Int']>;
-	perPage?: InputMaybe<Scalars['Int']>;
+  filter?: InputMaybe<UserListFilter>;
+  limit?: InputMaybe<Scalars['Int']>;
+  perPage?: InputMaybe<Scalars['Int']>;
 };
 
 export type StringFilter = {
-	and?: InputMaybe<StringFilter>;
-	beginsWith?: InputMaybe<Scalars['Boolean']>;
-	contains?: InputMaybe<Scalars['Boolean']>;
-	endsWith?: InputMaybe<Scalars['Boolean']>;
-	eq?: InputMaybe<Scalars['Boolean']>;
-	or?: InputMaybe<StringFilter>;
+  and?: InputMaybe<StringFilter>;
+  beginsWith?: InputMaybe<Scalars['Boolean']>;
+  contains?: InputMaybe<Scalars['Boolean']>;
+  endsWith?: InputMaybe<Scalars['Boolean']>;
+  eq?: InputMaybe<Scalars['Boolean']>;
+  or?: InputMaybe<StringFilter>;
 };
 
 export type Subscription = {
-	__typename?: 'Subscription';
-	error: Error;
+  __typename?: 'Subscription';
+  error: Error;
 };
 
 export type User = {
-	__typename?: 'User';
-	id: Scalars['ID'];
-	name: Scalars['String'];
+  __typename?: 'User';
+  id: Scalars['ID'];
+  name: Scalars['String'];
 };
 
 export type UserList = {
-	__typename?: 'UserList';
-	pagination?: Maybe<Pagination>;
-	users: Array<User>;
+  __typename?: 'UserList';
+  pagination?: Maybe<Pagination>;
+  users: Array<User>;
 };
 
 export type UserListFilter = {
-	and?: InputMaybe<UserListFilter>;
-	id?: InputMaybe<StringFilter>;
-	name?: InputMaybe<StringFilter>;
-	or?: InputMaybe<UserListFilter>;
+  and?: InputMaybe<UserListFilter>;
+  id?: InputMaybe<StringFilter>;
+  name?: InputMaybe<StringFilter>;
+  or?: InputMaybe<UserListFilter>;
 };
 
 export type UserLogin = {
-	password: Scalars['String'];
-	username: Scalars['String'];
+  password: Scalars['String'];
+  username: Scalars['String'];
 };
 
 export type UserToken = {
-	__typename?: 'UserToken';
-	token: Scalars['String'];
-	user?: Maybe<User>;
+  __typename?: 'UserToken';
+  token: Scalars['String'];
+  user?: Maybe<User>;
 };
 
 export type ListUsersQueryVariables = Exact<{
-	filter?: InputMaybe<UserListFilter>;
+  filter?: InputMaybe<UserListFilter>;
 }>;
 
-export type ListUsersQuery = {
-	__typename?: 'Query';
-	users?: {
-		__typename?: 'UserList';
-		users: Array<{ __typename?: 'User'; name: string; id: string }>;
-		pagination?: {
-			__typename?: 'Pagination';
-			itemsPerPage?: number | null;
-			totalItems?: number | null;
-		} | null;
-	} | null;
-};
 
-export const ListUsersDoc = gql`
-	query ListUsers($filter: UserListFilter) {
-		users(filter: $filter) {
-			users {
-				name
-				id
-			}
-			pagination {
-				itemsPerPage
-				totalItems
-			}
-		}
-	}
-`;
-export const ListUsers = (
-	options: Omit<WatchQueryOptions<ListUsersQueryVariables>, 'query'>
-): Readable<
-	ApolloQueryResult<ListUsersQuery> & {
-		query: ObservableQuery<ListUsersQuery, ListUsersQueryVariables>;
-	}
-> => {
-	const q = client.watchQuery({
-		query: ListUsersDoc,
-		...options
-	});
-	return readable<
-		ApolloQueryResult<ListUsersQuery> & {
-			query: ObservableQuery<ListUsersQuery, ListUsersQueryVariables>;
-		}
-	>({ data: {} as any, loading: true, error: undefined, networkStatus: 1, query: q }, (set) => {
-		q.subscribe((v: any) => {
-			set({ ...v, query: q });
-		});
-	});
+export type ListUsersQuery = { __typename?: 'Query', users?: { __typename?: 'UserList', users: Array<{ __typename?: 'User', name: string, id: string }>, pagination?: { __typename?: 'Pagination', itemsPerPage?: number | null, totalItems?: number | null } | null } | null };
+
+
+export const ListUsersDocument = gql`
+    query ListUsers($filter: UserListFilter) {
+  users(filter: $filter) {
+    users {
+      name
+      id
+    }
+    pagination {
+      itemsPerPage
+      totalItems
+    }
+  }
+}
+    `;
+
+export function useListUsersQuery(options?: Omit<Urql.UseQueryArgs<ListUsersQueryVariables>, 'query'>) {
+  return Urql.useQuery<ListUsersQuery>({ query: ListUsersDocument, ...options });
 };
