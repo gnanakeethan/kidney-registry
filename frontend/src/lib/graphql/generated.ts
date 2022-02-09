a6eaebd9f2bef2f166aeca383dd4e532b960c66e
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2022-02-09 09:17:24 +0530

    Creating query graphql and type & query generators
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

---
+++ frontend/src/lib/graphql/generated.ts
@@ -0,0 +1,133 @@
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type DashboardMenus = {
+  __typename?: 'DashboardMenus';
+  sidebarBottom?: Maybe<Menu>;
+  sidebarTop?: Maybe<Menu>;
+};
+
+export type Error = {
+  __typename?: 'Error';
+  status: Scalars['Int'];
+  string: Scalars['String'];
+};
+
+export type IntFilter = {
+  and?: InputMaybe<IntFilter>;
+  beginsWith?: InputMaybe<Scalars['Boolean']>;
+  endsWith?: InputMaybe<Scalars['Boolean']>;
+  eq?: InputMaybe<Scalars['Boolean']>;
+  gt?: InputMaybe<Scalars['Boolean']>;
+  gte?: InputMaybe<Scalars['Boolean']>;
+  lt?: InputMaybe<Scalars['Boolean']>;
+  lte?: InputMaybe<Scalars['Boolean']>;
+  or?: InputMaybe<IntFilter>;
+};
+
+export type Menu = {
+  __typename?: 'Menu';
+  items?: Maybe<Array<Maybe<MenuItem>>>;
+};
+
+export type MenuItem = {
+  __typename?: 'MenuItem';
+  icon?: Maybe<Scalars['String']>;
+  name?: Maybe<Scalars['String']>;
+  route?: Maybe<Scalars['String']>;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  error?: Maybe<Error>;
+  userLogin?: Maybe<UserToken>;
+};
+
+
+export type MutationUserLoginArgs = {
+  userLogin?: InputMaybe<UserLogin>;
+};
+
+export type Pagination = {
+  __typename?: 'Pagination';
+  currentPage?: Maybe<Scalars['Int']>;
+  itemsPerPage?: Maybe<Scalars['Int']>;
+  nextPage?: Maybe<Scalars['Int']>;
+  prevPage?: Maybe<Scalars['Int']>;
+  totalItems?: Maybe<Scalars['Int']>;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  error?: Maybe<Error>;
+  users?: Maybe<UserList>;
+};
+
+
+export type QueryUsersArgs = {
+  filter?: InputMaybe<UserListFilter>;
+  limit?: InputMaybe<Scalars['Int']>;
+  perPage?: InputMaybe<Scalars['Int']>;
+};
+
+export type StringFilter = {
+  and?: InputMaybe<StringFilter>;
+  beginsWith?: InputMaybe<Scalars['Boolean']>;
+  contains?: InputMaybe<Scalars['Boolean']>;
+  endsWith?: InputMaybe<Scalars['Boolean']>;
+  eq?: InputMaybe<Scalars['Boolean']>;
+  or?: InputMaybe<StringFilter>;
+};
+
+export type Subscription = {
+  __typename?: 'Subscription';
+  error: Error;
+};
+
+export type User = {
+  __typename?: 'User';
+  id: Scalars['ID'];
+  name: Scalars['String'];
+};
+
+export type UserList = {
+  __typename?: 'UserList';
+  pagination?: Maybe<Pagination>;
+  users: Array<User>;
+};
+
+export type UserListFilter = {
+  and?: InputMaybe<UserListFilter>;
+  id?: InputMaybe<StringFilter>;
+  name?: InputMaybe<StringFilter>;
+  or?: InputMaybe<UserListFilter>;
+};
+
+export type UserLogin = {
+  password: Scalars['String'];
+  username: Scalars['String'];
+};
+
+export type UserToken = {
+  __typename?: 'UserToken';
+  token: Scalars['String'];
+  user?: Maybe<User>;
+};
+
+export type ListUsersQueryVariables = Exact<{
+  filter?: InputMaybe<UserListFilter>;
+}>;
+
+
+export type ListUsersQuery = { __typename?: 'Query', users?: { __typename?: 'UserList', users: Array<{ __typename?: 'User', name: string, id: string }>, pagination?: { __typename?: 'Pagination', itemsPerPage?: number | null, totalItems?: number | null } | null } | null };
