3e11e733b3dabd927da510089514dd6da889a83c
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2022-02-16 11:58:36 +0530

    Removing unused statements
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

--- frontend/src/lib/guards/auth.ts
+++ frontend/src/lib/guards/auth.ts
@@ -7,16 +7,13 @@ authState.subscribe((authStateS) => (auth = authStateS));
 
 export async function authGuard({ url, params, props }: LoadInput): Promise<LoadOutput> {
 	const token = url.searchParams.get('token');
-	console.log(url, params, props, auth);
 	if (token !== undefined && token !== null && token.length > 32) {
 		authState.set({ loggedIn: true, token: token, loginAs: null });
-		console.log(base, '1');
 		return { status: 302, redirect: '/' };
 	}
 	if (auth.loggedIn && auth.token.length > 5) {
 		return {};
 	} else {
-		console.log(base, '3');
 		return { status: 302, redirect: base + '/auth/login' };
 	}
 }
