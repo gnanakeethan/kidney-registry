3dba0722191d748c751ef70f8b8de59a530fbb22
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2022-02-16 12:14:41 +0530

    Update: login logic
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

--- frontend/src/lib/guards/auth.ts
+++ frontend/src/lib/guards/auth.ts
@@ -2,18 +2,23 @@ import { base } from '$app/paths';
 import { LoadInput, LoadOutput } from '@sveltejs/kit';
 import { AuthState, authState } from '../state/auth';
 
-let auth: AuthState = { loggedIn: false, token: '', loginAs: null };
+let auth: AuthState = null;
 authState.subscribe((authStateS) => (auth = authStateS));
 
 export async function authGuard({ url, params, props }: LoadInput): Promise<LoadOutput> {
 	const token = url.searchParams.get('token');
 	if (token !== undefined && token !== null && token.length > 32) {
-		authState.set({ loggedIn: true, token: token, loginAs: null });
-		return { status: 302, redirect: '/' };
+		auth.token = token;
+		auth.loggedIn = true;
+		auth.redirectPage = url.pathname;
+		authState.set(auth);
+		return { status: 302, redirect: url.pathname };
 	}
 	if (auth.loggedIn && auth.token.length > 5) {
 		return {};
 	} else {
+		auth.redirectPage = url.pathname;
+		authState.set(auth);
 		return { status: 302, redirect: base + '/auth/login' };
 	}
 }
--- frontend/src/lib/state/auth.ts
+++ frontend/src/lib/state/auth.ts
@@ -1,25 +1,34 @@
-import {localStorage, persist, PersistentStore} from '@macfja/svelte-persistent-store';
-import {writable} from 'svelte/store';
+import { localStorage, persist, PersistentStore } from '@macfja/svelte-persistent-store';
+import { writable } from 'svelte/store';
 
 export interface AuthState {
-    loggedIn: boolean;
-    token: string;
-    loginAs: {
-        token: string,
-        displayName: string,
-        email: string,
-        userType: string,
-    }
+	loggedIn: boolean;
+	redirectPage: string;
+	token: string;
+	loginAs: {
+		token: string;
+		displayName: string;
+		email: string;
+		userType: string;
+	};
 }
 
-export const userType: PersistentStore<String> = persist(writable('consultant'), localStorage(), 'userType');
-export const authState: PersistentStore<AuthState> = persist(writable({
-    loggedIn: false,
-    token: '',
-    loginAs: {
-        token: '',
-        displayName: '',
-        email: '',
-        userType: '',
-    }
-} as AuthState), localStorage(), 'authState');
+export const userType: PersistentStore<string> = persist(
+	writable('consultant'),
+	localStorage(),
+	'userType'
+);
+export const authState: PersistentStore<AuthState> = persist(
+	writable({
+		loggedIn: false,
+		token: '',
+		loginAs: {
+			token: '',
+			displayName: '',
+			email: '',
+			userType: ''
+		}
+	} as AuthState),
+	localStorage(),
+	'authState'
+);
