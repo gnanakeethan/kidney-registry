6dcecac440a2fce44b16d58959407cae82223b77
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2022-02-17 08:52:55 +0530

    Update: check box
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

--- frontend/src/routes/recipients/view/[id]/examinations/index.svelte
+++ frontend/src/routes/recipients/view/[id]/examinations/index.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
 	import { recipientId } from '../../../../../lib/state/recipient';
+
+	function checkedClick(i) {
+		checked[i] = checked[i] !== undefined ? !checked[i] : true;
+	}
+
+	let checked = [];
 </script>
 
 <div class="p-2">
@@ -7,41 +13,27 @@
 		DONORS {$recipientId}
 		<a href="/recipients/view/{$recipientId}/examinations/{$recipientId}">Donor</a>
 	</div>
-	<style>
-		.select-size input {
-			display: none;
-		}
-
-		#small:checked ~ label[for='small'],
-		#medium:checked ~ label[for='medium'],
-		#large:checked ~ label[for='large'],
-		#x-large:checked ~ label[for='x-large'],
-		#xx-large:checked ~ label[for='xx-large'] {
-			background: rgb(44, 3, 226);
-			color: #fff;
-		}
-	</style>
-	<div class="select-size">
-		<input checked id="medium" name="s-size" type="checkbox" />
-		<input id="large" name="s-size" type="checkbox" />
-		<input id="x-large" name="s-size" type="checkbox" />
-		<input id="xx-large" name="s-size" type="checkbox" />
-
-		<label
-			class="inline-block  cursor-pointer rounded-full border px-2 py-1 text-center"
-			for="medium">Headache</label
-		>
-		<label
-			class="inline-block  cursor-pointer rounded-full border px-2 py-1 text-center"
-			for="large">Fits</label
-		>
-		<label
-			class="inline-block  cursor-pointer rounded-full border px-2 py-1 text-center"
-			for="x-large">Weakness</label
-		>
-		<label
-			class="inline-block  cursor-pointer rounded-full border px-2 py-1 text-center"
-			for="xx-large">Numbness</label
-		>
+	<div class="select-size flex flex-row">
+		{#each { length: 10 } as _, i}
+			<div>
+				<input type="checkbox" bind:checked={checked[i]} id={i} />
+				<label
+					for={i}
+					class="inline-block  cursor-pointer rounded-full border px-2 py-1 text-center"
+				>
+					Headache {i}
+				</label>
+			</div>
+		{/each}
 	</div>
 </div>
+
+<style>
+	input[type='checkbox'] {
+		display: none;
+	}
+
+	input[type='checkbox']:checked + label {
+		@apply bg-green-500 text-white;
+	}
+</style>
