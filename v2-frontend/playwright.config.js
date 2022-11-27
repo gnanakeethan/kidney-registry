60b2fdec1cb1e71c7e21a457ccddc9cbd8b68d6e
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2022-11-27 19:51:45 +0530

    Updating
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

---
+++ v2-frontend/playwright.config.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var config = {
+    webServer: {
+        command: 'npm run build && npm run preview',
+        port: 4173
+    }
+};
+exports["default"] = config;
+//# sourceMappingURL=playwright.config.js.map
--- v2-frontend/svelte.config.js
+++ v2-frontend/svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 			{
 				fallback: 'index.html'
 			}
-		)
+		),
 	}
 };
 
