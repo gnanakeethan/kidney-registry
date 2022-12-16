09582fae8c5cf9dd1e66b67ed5dbc11ee1ac0caa
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2022-12-16 07:48:02 +0530

    Fixing out
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

---
+++ v2-frontend/houdini.config.js
@@ -0,0 +1,11 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "schemaPath": "../graph/schema/*.graphql",
+    "plugins": {
+        "houdini-svelte": {}
+    }
+}
+
+export default config
