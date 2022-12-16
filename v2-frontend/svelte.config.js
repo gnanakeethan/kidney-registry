b4ef1939d5940ac5c7e1283dbf3d3966e53bc2f3
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2022-12-16 07:53:19 +0530

    Reverting issues
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

--- v2-frontend/svelte.config.js
+++ v2-frontend/svelte.config.js
@@ -1,16 +1,18 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import houdini from 'houdini-svelte/preprocess';
+import * as path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [houdini(), vitePreprocess()],
 
 	kit: {
 		adapter: adapter(),
 		alias: {
-			$houdini: './$houdini'
+			$houdini: path.resolve('.', '$houdini')
 		}
 	}
 };
--- v2-frontend/vite.config.ts
+++ v2-frontend/vite.config.ts
@@ -1,9 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import houdini from 'houdini/vite';
-import type { UserConfig } from "vite";
+import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
 	plugins: [houdini(), sveltekit()],
-}
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+};
 
 export default config;
